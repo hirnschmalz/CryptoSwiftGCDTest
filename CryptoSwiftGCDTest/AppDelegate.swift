007b5fc84e6b125b84c2c65b6a475bb67afc1ecd
Markus Bischof <markus@doerler.co.at>
2014-12-27 08:41:52 +0100

    Initial Commit

---
+++ CryptoSwiftGCDTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CryptoSwiftGCDTest
+//
+//  Created by Markus Bischof on 27.12.14.
+//  Copyright (c) 2014 Hirnschmalz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CryptoSwiftGCDTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CryptoSwiftGCDTest
+//
+//  Created by Markus Bischof on 27.12.14.
+//  Copyright (c) 2014 Hirnschmalz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
